934a387beaca0b7fb840fd5cf887890a3c9ef799
Vova <vir42@mail.ru>
2020-07-11 14:36:34 +0300

    feat: add page

---
+++ precache-manifest.3d0dd97f4e056c5582bb1ae02071e5cb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6ccf9b0a00ba507d8279e6e55f1891",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ca8e1479af7a775a5af1",
+    "url": "/static/css/2.52f2db83.chunk.css"
+  },
+  {
+    "revision": "7bc87246b89ad10e1958",
+    "url": "/static/css/main.7b9176a0.chunk.css"
+  },
+  {
+    "revision": "ca8e1479af7a775a5af1",
+    "url": "/static/js/2.a5c9f6da.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a5c9f6da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bc87246b89ad10e1958",
+    "url": "/static/js/main.2522c4d1.chunk.js"
+  },
+  {
+    "revision": "8c05a4a0ac9efca20d9e",
+    "url": "/static/js/runtime-main.c36b7619.js"
+  },
+  {
+    "revision": "10b9cd1ddd69d318220fdfeca35a23b3",
+    "url": "/static/media/Gilroy-Bold.10b9cd1d.ttf"
+  },
+  {
+    "revision": "ef9daf9c32f47a3468f30f861e92b9c4",
+    "url": "/static/media/Gilroy-Medium.ef9daf9c.ttf"
+  },
+  {
+    "revision": "db122e773d3c84ffe43b343ead3c6773",
+    "url": "/static/media/arrow.db122e77.svg"
+  },
+  {
+    "revision": "54d7e65e7155296b9ee76b56e17798b0",
+    "url": "/static/media/ellips.54d7e65e.png"
+  },
+  {
+    "revision": "d91f471b94e2ec9a6e5231a304dc0764",
+    "url": "/static/media/firstSky.d91f471b.png"
+  },
+  {
+    "revision": "b2638ee10fe1cd6e66e7cb34769485da",
+    "url": "/static/media/forth.b2638ee1.png"
+  },
+  {
+    "revision": "67b424967720e3f1c5e1422a33715ea5",
+    "url": "/static/media/imageBackGround.67b42496.png"
+  },
+  {
+    "revision": "960210178ff7b889ecdc3e2fcc202adc",
+    "url": "/static/media/leftImage.96021017.png"
+  },
+  {
+    "revision": "25836a074dad0cefb1c49eb979c2f778",
+    "url": "/static/media/rightImage.25836a07.png"
+  },
+  {
+    "revision": "ad2e6ebae80536ba8c3bf2322733e549",
+    "url": "/static/media/secondSky.ad2e6eba.png"
+  },
+  {
+    "revision": "520e6af3921dd9ffd3651d3892c05d82",
+    "url": "/static/media/thtirdSky.520e6af3.png"
+  }
+]);
